92e493a057aea9fed9b3d94358caccc9c8b9d49d
Robert Stein <robert.stein@desy.de>
2019-10-29 12:46:07 +0100

    Try pickle, otherwise .txt, for loading scan results.

--- full_parsing_chain.py
+++ full_parsing_chain.py
@@ -19,7 +19,10 @@ if __name__ == "__main__":
         candidates = [y for y in os.listdir(args.cache_dir) if "event" in y]
 
     for candidate in candidates:
-        cand_name = parse_archival_txt(candidate, args.output_dir, args.cache_dir)
+        try:
+            cand_name = parse_archival_scan(candidate, args.output_dir, args.cache_dir)
+        except IOError:
+            cand_name = parse_archival_txt(candidate, args.output_dir, args.cache_dir)
         add_contextual_info(cand_name, args.output_dir)
         convert_to_equatorial(cand_name, args.output_dir)
         convert_llh_to_prob(cand_name, args.output_dir)
